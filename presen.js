ef927cd8cf04c298a1f64b73c8d075c24b4a15f8
thai150423 <124511312+thai150423@users.noreply.github.com>
2023-02-05 21:41:11 +0700

    Add files via upload
    create card

---
+++ presen.js
@@ -0,0 +1,44 @@
+var to = 'Vô dụng';
+var gift_url = 'https://www.facebook.com/uyen.ane.7?mibextid=LQQJ4d';
+var gift_image_url = 'ảnh1.jpg';
+
+
+
+var nametag = document.getElementById("nametag");
+var present = document.getElementById("present");
+var presentImage = document.getElementById("present-image");
+
+
+function init() {
+  
+  var _giftLink, 
+      _giftImg;
+  
+  if (gift_url) {
+    _giftLink = document.createElement("a");
+    _giftLink.href = gift_url;
+    _giftLink.target = "_blank";
+    presentImage.appendChild(_giftLink);
+  }
+  
+  if (gift_image_url) {
+    _giftImg = document.createElement("img");
+    _giftImg.src = gift_image_url;
+    if(_giftLink) {
+      _giftLink.appendChild(_giftImg);
+    } else {
+      presentImage.appendChild(_giftImg);
+    }
+  }
+    
+  present.addEventListener("click", function(e){
+    present.classList.toggle("open");
+    document.getElementById('card').classList.add('snu-show');
+  }, false);
+  
+  
+  
+  nametag.innerText = to;
+}
+
+init();
